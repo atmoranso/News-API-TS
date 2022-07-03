118edba7506048300311506408b1ce9da9eb0021
Atmoranso <atmoranso@gmail.com>
2022-07-03 23:05:28 +0300

    feat: add API key

--- migration-newip-to-ts/src/components/controller/appLoader.js
+++ migration-newip-to-ts/src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: '6d44acaa25434bd8a88cd67d5b43231c', // получите свой ключ https://newsapi.org/
         });
     }
 }
